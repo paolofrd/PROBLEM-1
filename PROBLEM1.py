8001329e94d95986fe93076660d9574a2dfee149
Paolo Fajardo <57589064+paolofrd@users.noreply.github.com>
2019-12-08 15:28:39 +0800

    Add files via upload

---
+++ PROBLEM1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+n = 0
+y = []
+x = list(range(0,100))
+for n in range (0,100):
+    if n <= 9:
+        i = (n*n)-7
+        y.append(i)
+    elif n >= 10:
+        while n >= 10:
+            n = n-10
+            if n <= 9:
+                i = (n*n)-7
+                y.append(i)
+plt.stem(x,y,use_line_collection=True)
+plt.show() 
